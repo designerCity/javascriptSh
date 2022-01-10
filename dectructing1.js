dcae493b5175237d772f4f9a14880b4a91e4faab
designerCity <97098829+designerCity@users.noreply.github.com>
2022-01-10 19:34:11 +0900

    Add files via upload
    구조 분해 (destructing)
    desturcting 을 이용한 변수 교환

---
+++ dectructing1.js
@@ -0,0 +1,32 @@
+// 구조 분해 (destructing)
+
+const rank = ['세형', '지완', '준성', '진혁']
+
+// const samsong = rank[0];
+// const mac = rank[1]
+// const [samsong, mac, ipad] = rank;
+// 배열의 길이가 넘치는 요소는 순서대로 정렬이 된다. 
+// 배열의 길이가 더 짧으면 return 이 undefined 가능
+
+// 기본값 설정 가능하다.
+const [samsong, mac, ipad = '없음'] = rank;
+
+// const [samsong, mac, ...ipad] = rank;
+// rest parameter 처럼 나머지 것들을 할당한다.
+
+console.log(samsong)
+console.log(mac)
+console.log(ipad)
+
+// 변수 교환
+let macbook = '지완';
+let ipod = '세형';
+
+console.log('macbook 소유', macbook);
+console.log('ipod 소유', ipod);
+
+// destructing 을 이용한 변수 교환s
+[macbook, ipod] = [ipod, macbook]
+
+console.log('macbook 소유', macbook);
+console.log('ipod 소유', ipod);
