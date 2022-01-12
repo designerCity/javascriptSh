71da88fd6a81e1452b5a64caf4636bad6977209a
designerCity <97098829+designerCity@users.noreply.github.com>
2022-01-12 13:48:48 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const list = document.querySelector('.list');
-const data = [{
-    title: '자바스크립트 공부하기',
-    isClear: true,
-  }, {
-    title: '독서하기',
-    isClear: false,
-  }, {
-    title: '수학 과외하기',
-    isClear: true,
-  }, {
-    title: '독서하기',
-    isClear: true,
-  }, {
-    title: '영어 공부하기',
-    isClear: false,
-  }
+const nameInput = document.querySelector('#user-name');
+const phoneInput = document.querySelector('#phone-number');
+const findBtn = document.querySelector('#find');
+
+const data = [
+  { userName: '막대기', phoneNumber: '01012341111', email: 'stick@go_do_it.kr' },
+  { userName: 'young', phoneNumber: '01012342222', email: 'kang@go_do_it.kr' },
+  { userName: '코린이', phoneNumber: '01012343333', email: 'corin2@go_do_it.kr' },
+  { userName: 'captain', phoneNumber: '01012344444', email: 'crew@go_do_it.kr' },
+  { userName: 'YH', phoneNumber: '01012345555', email: 'whyH@go_do_it.kr' },
+  { userName: '망고쥬스', phoneNumber: '01012346666', email: 'drinkMango@go_do_it.kr' },
+  { userName: 'nemoming', phoneNumber: '01012347777', email: 'ractAngle@go_do_it.kr' },
+  { userName: '강그루', phoneNumber: '01012348888', email: 'riverTree@go_do_it.kr' },
+  { userName: '개룩발룩', phoneNumber: '01012349999', email: 'checkShirts@go_do_it.kr' },
+  { userName: '오렌지쥬스', phoneNumber: '01012341010', email: 'delmonte@go_do_it.kr' },
 ];
 
+function findEmail() {
+  const nameValue = nameInput.value;
+  const phoneValue = phoneInput.value;
+  console.log(nameValue)
+  // 여기에 코드를 작성해 주세요.
+  const user = data.find((el) => el.userName === nameValue && el.phoneNumber === phoneValue);
 
-data.forEach((todo, i) => { 
-  const li = document.createElement('li'); // 새로운 list 를 추가하기 위해서 index.html 의 <li> 를 생성
-  
-  if (todo.isClear) {
-    li.classList.add('item', 'done'); // 생성한 <li> 가 isClear === true 면 'item', 'done' 두 class 를 추가 
-  } else {
-    li.classList.add('item'); // 생성한 <li> 가 isClear === false 면 'item' class 를 추가 
-  }
 
-  li.textContent = `${i + 1}. ${todo.title}`; // <li> 안에 들어갈 컨텐츠를 생성하여 li.textContent 를 이용해 집어넣어주고 
-  list.appendChild(li); // 마지막에 반복적으로 집어 넣으면서 순서대로 정렬되기끔 
-});
+  const message = user
+    ? `${user.userName}님의 이메일은 ${user.email} 입니다.`
+    : '이메일을 찾을 수 없습니다. 입력 정보를 다시 확인해 주세요.';
+
+  alert(message);
+}
+
+findBtn.addEventListener('click', findEmail);
