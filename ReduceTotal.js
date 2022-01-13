2a04acbf2e163b3321e480344f5fd37248ed7ebf
designerCity <97098829+designerCity@users.noreply.github.com>
2022-01-14 01:26:28 +0900

    Add files via upload

---
+++ ReduceTotal.js
@@ -0,0 +1,15 @@
+const data = [ 
+  { company: 'Naber', month: 3 },
+	{ company: 'Amajohn', month: 12 },
+	{ company: 'Coogle', month: 10 },
+  { company: 'Ittel', month: 6 },
+  { company: 'Sasung', month: 4 },
+  { company: 'CaCao', month: 3 },
+	{ company: 'Microhard', month: 17 },
+];
+
+const totalCareer = data.reduce((acc, el, i) => {
+  return el.month + acc;
+},0)
+
+console.log(`세형이의 경력은 총 ${totalCareer}개월입니다.`);
