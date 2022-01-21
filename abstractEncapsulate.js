f069dd67b586208a29fe15f0d2f70cd8c819704b
designerCity <97098829+designerCity@users.noreply.github.com>
2022-01-21 23:08:23 +0900

    Add files via upload
    객체 지향 프로그래밍 핵심 개념 4가지 기둥

---
+++ abstractEncapsulate.js
@@ -0,0 +1,45 @@
+// 객체 지향 프로그래밍 핵심 개념 4가지 기둥 
+// 추상화 : 어떤 구체적인 존재를 원하는 방향으로 간략화해서 나타내는 것
+          // 주의점 : 협업할 수 있게 naming 을 하는 것이 좋다.
+
+// 캡슐화 : 객체의 특정 프로퍼티에 직접 접근하지 못하도록 막는 것 
+          // 협업을 하는 사람들이 객체 내부의 프로퍼티 값을 이상하게 설정하는 것을 막을 수 있다. 
+          // 이를 통해 프로그램의 안정성을 높일 수 있다. 
+class User {
+  constructor(email, birthdate) {
+    this.email = email;
+    this.birthdate = birthdate;
+  }
+  
+  buy(item) {
+    console.log(`${this.email} buys ${item.name}`);
+  }
+  // getter method 는 프로퍼티의 값을 구하는 메소드 
+  // 프로퍼티를 읽는 용도로 사용된다. 
+  get email() {
+    return `Email Address is ${this._email}`;
+  }
+  
+  // setter method
+  // 프로퍼티(email)에 접근하는 것을 막는 방법 => 접근 시 이 함수가 실행됨.
+  set email(address) {
+    if (address.includes('@')) { // 값에 대한 유효성 검사
+      this._email = address; // 숨기고자하는 프로퍼티 이름은 맨앞에 _를 붙이는 경우가 많다.
+    } else {
+      throw new Error('invalid email address')
+    }
+  }
+}
+
+const item = {
+  name: '스웨터',
+  price: 30000,
+};
+
+const user1 = new User('tpgud777@nate.com', '1999-04-25');
+user1.email = 'SeHyeung@nate.com'
+console.log(user1._email); // SeHyeung@nate.com
+console.log(user1.email); // Email Address is SeHyeung@nate.com
+// console.log(user1.email) 는 getter method 가 실행됨으로 인해 결과값이 다르다. 
+console.log(constructor)
+
---
+++ inheritMultiForm.js
@@ -0,0 +1,81 @@
+// 상속 : 하나의 객체가 다른 객체의 프로퍼티와 메소드를 물려받는 것
+  // 장점 : 코드의 재사용성이 좋아진다. 
+// 부모 class : base 가 되는 class 
+class User {
+  constructor(email, birthdate) {
+    this.email = email;
+    this.birthdate = birthdate;
+  }
+  
+  buy(item) {
+    console.log(`${this.email} buys ${item.name}`);
+  }
+}
+// 상속하는 법 extends 
+// 자식 class (derived class): 상속 받는 class
+class PremiumUser extends User {
+  constructor(email, birthdate, level) {
+    // this.email = email;
+    // this.birthdate = birthdate;
+    super(email, birthdate) // 자식 class 로 객체 생성할 때는 반드시 그 생성자 함수 내에세 super 호출하여
+    // 부모 class의 생성자 함수를 먼저 호출해야한다.
+    this.level = level;
+  }
+  streamMusicForFree() {
+    console.log(`Free music streaming for ${this.email}`)
+  }
+  // buy(item) {
+  //   console.log(`${this.email} buys ${item.name}`);
+  // }
+}
+const item = {
+  name: '스웨터',
+  price: 30000,
+}
+
+const pUser1 = new PremiumUser('tpgud777@nate.com', '1999-04-25', 3)
+console.log(pUser1.email);
+console.log(pUser1.birthdate);
+console.log(pUser1.level);
+pUser1.buy(item);
+pUser1.streamMusicForFree();
+
+
+
+// 다형성 : 많은 형태를 갖고 있는 성질, 하나의 변수가 다양한 종류의 객체를 가리킬 수 있는 성질. 
+
+class User {
+  constructor(email, birthdate) {
+    this.email = email;
+    this.birthdate = birthdate;
+  }
+  
+  buy(item) {
+    console.log(`${this.email} buys ${item.name}`);
+  }
+}
+class PremiumUser extends User {
+  constructor(email, birthdate, level) {
+    super(email, birthdate)
+    this.level = level;
+  }
+  // 오버라이딩(overriding) : 자식 class 에서 부모에 있는 동일한 method를 정의하고
+  // 그 내용을 다르게 채우는 것.
+  // buy method 를 수정하고 싶을 때
+  buy(item) {
+    console.log(`${this.email} buys ${item.name} with a 5% discount`)
+  }
+  streamMusicForFree() {
+    console.log(`Free music streaming for ${this.email}`)
+  }
+}
+const item = {
+  name: '스웨터',
+  price: 30000,
+}
+
+const user1 = new User('tpgud777@nate.com', '19990425')
+const pUser1 = new PremiumUser('tpgud777@nate.com', '19990425')
+
+user1.buy(item)
+pUser1.buy(item)
