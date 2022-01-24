9aefb45159796d25c5c5f2965efc47c066d3222c
designerCity <97098829+designerCity@users.noreply.github.com>
2022-01-24 17:17:11 +0900

    Create webBasic.js

---
+++ webBasic.js
@@ -0,0 +1,37 @@
+// WEB? : 전 세계적인 연결망 
+// Hyper Text : Web page 에 적혀있는 text 
+// HTML ( Hyper Text Markup Language )
+
+
+// URL ( Uniform Resource Locator ) : 웹에 존재하는 특정 데이터를 나타내는 문자열 
+
+
+// Host : 전 세계 서버 중 하나의 서버를 특정 
+// ex ) www.google.com (Domain Name도메인네임)
+//  Domain Name System이라고 하는 체계를 바탕으로 생성되는 문자열 이름으로, 
+//  인터넷 세계에 존재하는 수많은 영역을 추상적으로 나타내기 위해 사용되는 개념
+//  하나의 도메인 네임은 여러 개의 도메인으로 구성됩니다. 그리고 더 상위의 도메인일수록 도메인 네임 중에서 오른쪽
+
+// // DOMAIN 의 순서 및 종류 
+//  최상단에 루트 도메인(root domain)
+
+//  Top-Level Domain(TLD) : 사이트가 속한 국가 또는 사이트가 제공하는 서비스의 카테고리
+//  ex ) .kr, .jp, .net, .gov 
+
+//      172.217.175.100 ( IP 주소 )
+
+// Path : 서버에 있는 데이터 중 원하는 데이터를 특정 
+// 표현 방식은 만드는 개발자들이 만드는 영역이다. 
+// ex ) /men/shirts
+
+// Query : 데이터에 관한 세부적인 요구사항
+// 존재할 때도 있고 존재하지 않을 때도 있따. 
+// ?color=blue&size=m
+
+
+// 스킴 ( Scheme ) : 프로토콜의 이름이 들어간다. 
+                      // (protocol) : 통신을 하는 두 주체가 지켜야 하는 통신 규약
+// https 는 프로토콜의 이름       
+// http (Hyper Text Transfer Protocol)
+// https (Hyper Text Transfer Protocol Secure)
+    // Hyper Text : 다른 텍스트에 대한 참조를 갖고 있는 text 
