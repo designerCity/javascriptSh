0aa16d2f22a8e945903f0fd45859f6116a45efb7
designerCity <97098829+designerCity@users.noreply.github.com>
2022-01-24 17:14:21 +0900

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -11,3 +11,7 @@ fetch('https://www.google.com/') // fetch 는 가져오다라는 뜻
   .then((result) => { console.log(result); }); // 이전 콜백의 retrun 값(response.text())을 
                                                // 다음 callback 이 넘겨 받을 수 있다 (result). 
   
+
+// fetch 함수는 어떤 객체를 리턴하는데(Promise 객체, 챕터 3에서 배웁니다)
+// 이 객체의 then 메소드로, '리스폰스가 왔을 때 실행할 콜백'을 등록할 수 있습니다.
+// 이렇게 등록된 콜백들은 then 메소드로 등록한 순서대로 실행되고, 이때 이전 콜백의 리턴값을 이후 콜백이 넘겨받아서 사용할 수 있는데요.
