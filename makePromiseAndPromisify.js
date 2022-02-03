fa411f7cd6e3da746d15a01dd128151ab117f0f3
designerCity <97098829+designerCity@users.noreply.github.com>
2022-02-03 19:26:24 +0900

    Update makePromiseAndPromisify.js

--- makePromiseAndPromisify.js
+++ makePromiseAndPromisify.js
@@ -33,6 +33,28 @@ function wait(text, millseconds) {
 }
 
 fetch('https://jsonplaceholder.typicode.com/users')
+// -------------------------------------------------------------------------------------------------------------------
+
+// 이미 상태가 결정된 promise object 만들기 
+
+// // 1. fullfilled 상태의 promise object 만들기 
+const p = promise.resolve('success');
+
+// fullfilled 상태이면서
+// 작업 성공 결과 : 'success' 인 Promise obejct 
+
+// // 2. rejected 상태의 Promise object 만들기
+const p = Promise.reject(new Error('fail'));
+
+// rejected 상태이면서
+// 작업 실패 정보로 fail 이라는 메시지를 가진 Error object 를 가진 Promise object 를 만들 수 있다. 
+
+
+// 어떤 비동기 작업을 처리할 필요가 있다면, new 생성자와 executor 함수를 사용해 Promise object 를 만들어야함.
+// 바로 상태가 이미 결정된 Promise object 를 만들고 싶을 땐 resolve 또는 reject method 를 사용한다. 
+
+// Promise object 의 작업 성공 결과 또는 작업 실패 정보는 시점과는 전혀 상관이 없는 개념이다. 
+// Promise 객체는 항상 결과를 줄 수 있는 공급자(Provider)이고 그것의 then 메소드는 그 결과를 소비하는 콜백인 소비자(Consumer)를 설정하는 메소드라는 사실
   .then((response) => response.text())
   .then((result) => wait(`${result} by Codeit`, 2000)) // 2초 후에 리스폰스의 내용 뒤에 'by Codeit' 추가하고 리턴
   .then((result) => { console.log(result); });
